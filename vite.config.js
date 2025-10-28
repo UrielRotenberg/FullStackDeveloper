1bcbbbc42362a7a2c2bc21c040a5304374f007c1
UrielRotenberg <uri314el@gmail.com>
2025-10-28 22:08:43 +0200

    עדכון base לפי סביבה + הגדרת basename ל-React Router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// מחזיר base שונה לפי הסביבה
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/',
-})
+  base: mode === 'development' ? '/' : '/FullStackDeveloper/',
+}))
